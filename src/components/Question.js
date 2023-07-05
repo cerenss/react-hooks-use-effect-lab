1455021e7a1d1ea0d53bd433f9312345aded8fe0
Ceren Şahin <cerennssahinn@gmail.com>
2023-07-05 13:35:32 +0300

    Completed assignment. onAnswered(false) after 10 seconds is failing the test. Couldn't fix.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const setTime = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+      if (timeRemaining === 0) {
+        console.log(timeRemaining);
+        onAnswered(false);
+        setTimeRemaining(10)
+      }
+    }, 1000)
+
+     return () => {
+      clearTimeout(setTime)
+    } 
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
